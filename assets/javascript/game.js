e8538511f68ccc9c348092620811b8ea3c8489c8
ppreziotti <patpreziotti@gmail.com>
2017-03-10 23:01:57 -0500

    added html,css,js

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var userScore = 0;
+var wins = 0;
+var losses = 0;
+var targetScore = Math.floor(Math.random() * (120 - 19));
+
+console.log(targetScore);
+
+$("#target-score").html("Score to Match: " + targetScore);
+
